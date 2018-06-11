3b965a3935f9d9b896405985556b6a06f95434fe
xingkongxia <690174435@qq.com>
2018-06-11 19:04:07 +0800

    fix test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,27 +4,12 @@
 // find child widgets in the widget tree, read text, and verify that the values of widget properties
 // are correct.
 
-import 'package:flutter/material.dart';
 import 'package:chic/widget/home.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:chic/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(new HomePage());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
   });
 }
